7fc38150562bba8d8b92bab03e266141f2b844c9
Geraldo Otávio Figueiredo <geraotavio@hotmail.com>
2024-01-08 19:10:03 -0300

    change background based on media queries

--- script.js
+++ script.js
@@ -7,24 +7,22 @@ function toggleMode() {
   //    html.classList.add('light')
   //  }
 
-  html.classList.toggle('light')
-//pegar a tag img
+  html.classList.toggle("light")
+  //pegar a tag img
   const img = document.querySelector("#profile img")
   //substituir a imagem
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     //se tiver em light mode, usar a imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
- } else {
-    // se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', './assets/gerah.imageredonda.png')
- }
-
-  const alt = document.querySelector("#profile img")
-  if(html.classList.contains('light')) {
-    img.setAttribute('alt', "Foto de Geraldo modo Light")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute('alt', "Foto de Geraldo")
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/gerah.imageredonda.png")
   }
 
-
+  const alt = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Foto de Geraldo modo Light")
+  } else {
+    img.setAttribute("alt", "Foto de Geraldo")
+  }
 }
